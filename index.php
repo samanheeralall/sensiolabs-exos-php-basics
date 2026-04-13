095935fbefb03b956010af97c2be95116a972ff9
Heeralall <heeralall@MacBook-Air-de-Heeralall.local>
2026-04-13 15:10:07 +0200

    feat(index): correctif

--- index.php
+++ index.php
@@ -6,16 +6,16 @@ require_once __DIR__.'/EventDispatcher.php';
 $dispatcher = new EventDispatcher();
 
 // Listener avec callable (exercice 1)
-$dispatcher->addListener(eventName: 'event_foo', listener: function($event) {
+$dispatcher->addListener('event_foo', function($event) {
     echo 'Event Foo dispatched!'.PHP_EOL;
 });
 
 // Listener avec interface (exercice 2) — classe anonyme
-$dispatcher->addListener(eventName: 'event_foo', listener: new class implements EventListenerInterface {
+$dispatcher->addListener('event_foo', new class implements EventListenerInterface {
     public function handle(object $event): void
     {
         echo 'Event Foo listened from Interface'.PHP_EOL;
     }
 });
 
-$dispatcher->dispatch(new stdClass(), eventName: 'event_foo');
+$dispatcher->dispatch(new stdClass(),'event_foo');
